3375e02356b043cf96823bf7d69264a73cd8baf7
oDen7 <dengao99@126.com>
2019-03-03 23:19:56 +0800

    fix

--- js/details.js
+++ js/details.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-  $("#li1").click(function() { // 根据 id 拿到 dom节点  做 事件（点击事件） 干什么事情  下面
-    $("#childUl1").stop().slideToggle(); //  取反  显示时 点击之后  隐藏  反之  显示
-  });
+  $(document).ready(function() {
+    $("#li1").click(function() { // 根据 id 拿到 dom节点  做 事件（点击事件） 干什么事情  下面
+      $("#childUl1").stop().slideToggle(); //  取反  显示时 点击之后  隐藏  反之  显示
+    });
 
-  $("#li2").click(function() {
-    $("#childUl2").stop().slideToggle();
-  });
+    $("#li2").click(function() {
+      $("#childUl2").stop().slideToggle();
+    });
 
-  var tabList = $(".tabList > li");
-  for (var i = 0; i < tabList.length; i++) {
-    $(tabList[i]).attr("id", "tab" + i);
-  }
+    var tabList = $(".tabList > li");
+    for (var i = 0; i < tabList.length; i++) {
+      $(tabList[i]).attr("id", "tab" + i);
+    }
 
-})
+  })
