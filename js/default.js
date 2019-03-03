4945750c4e88253f5edb613df27c08611ed55c1d
oDen7 <dengao99@126.com>
2019-03-03 21:23:32 +0800

    fix

--- js/default.js
+++ js/default.js
@@ -1,14 +1,14 @@
-$(document).ready(function () {
-    var lis = $("li");
-    var list = $("#sortList > li"); // 拿到  一级 li 
-    var childUl = $("#sortList > li >ul"); // 拿到  一级 li  下的 ul
-    var childList = $("#sortList > li >ul > li");
+$(document).ready(function() {
+  var lis = $("li");
+  var list = $("#sortList > li"); // 拿到  一级 li
+  var childUl = $("#sortList > li >ul"); // 拿到  一级 li  下的 ul
+  var childList = $("#sortList > li >ul > li");
 
-    for (var i = 0; i < lis.length; i++) { // 循环遍历 
-        $(list[i]).attr("id", "li" + i); // 动态添加 属性  key：id  value： 名称  + 循环 i 变量
-        $(childUl[i]).attr("id", "childUl" + i); // 同上
-        $(childList[i]).attr("id", "childList" + i); // 同上
-    }
+  for (var i = 0; i < lis.length; i++) { // 循环遍历
+    $(list[i]).attr("id", "li" + i); // 动态添加 属性  key：id  value： 名称  + 循环 i 变量
+    $(childUl[i]).attr("id", "childUl" + i); // 同上
+    $(childList[i]).attr("id", "childList" + i); // 同上
+  }
 });
 
 
@@ -20,11 +20,11 @@ $(document).ready(function () {
 
 var string = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-/* 
-   循环遍历之后，删除 需要删除的 数据  
+/*
+   循环遍历之后，删除 需要删除的 数据
 *  如果单纯使用 for循环 遍历数据 效率会很低 （顺序查找）
 *   当数据量较大时 会使查询 时间 增大
-*    
+*
 *   下列算法使用  二分排序 + 递归 ，大O 计法  log2（n）
 *
 * 使用方式
@@ -32,12 +32,12 @@ var string = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "
 *   string.splice(result, 1);
 */
 function removeAdata(stringArr, queryString, low, high) {
-    var mid = Math.floor((high - low) / 2 + low);
-    if (queryString < stringArr[mid]) {
-        return removeAdata(stringArr, queryString, low, mid - 1);
-    } else if (queryString > stringArr[mid]) {
-        return removeAdata(stringArr, queryString, mid + 1, high);
-    } else {
-        return mid;
-    }
+  var mid = Math.floor((high - low) / 2 + low);
+  if (queryString < stringArr[mid]) {
+    return removeAdata(stringArr, queryString, low, mid - 1);
+  } else if (queryString > stringArr[mid]) {
+    return removeAdata(stringArr, queryString, mid + 1, high);
+  } else {
+    return mid;
+  }
 }
--- js/shop.js
+++ js/shop.js
@@ -1,39 +1,38 @@
-$(document).ready(function () {
-    $("#li1").click(function () { // 根据 id 拿到 dom节点  做 事件（点击事件） 干什么事情  下面
-        $("#childUl1").stop().slideToggle(); //  取反  显示时 点击之后  隐藏  反之  显示
+$(document).ready(function() {
+  $("#li1").click(function() { // 根据 id 拿到 dom节点  做 事件（点击事件） 干什么事情  下面
+    $("#childUl1").stop().slideToggle(); //  取反  显示时 点击之后  隐藏  反之  显示
+  });
+
+  $("#li2").click(function() {
+    $("#childUl2").stop().slideToggle();
+  });
+
+  var picList = $("#picList >li");
+  var pointList = $("#pointList>li");
+  var low = 0; //  设置全局变量  为 轮播图 的  绝对定位 做准备
+
+  function autoPlay() { //   自动 轮播  方法/函数
+    low = low - 1047; // 将  low  的 值   每次  减少  1047  注：1047为一张图片的宽度
+    $(".picList").stop().animate({ //  拿到所有的 class 为  .picList 的  节点  先执行  停止动画  重置  动画队列  否则 动画会有冲突 按照队列执行 再执行动画
+      "left": low + "px" //  将  low  变量的值  动态 赋值给  css left  注：.picList  为绝对定位 相对于 lunboComponent
     });
+    if (low == -4188) { //  边界值 检测  当  Low 为 -4188 时  为 五张图片总长度  执行函数体语句
+      return low = 1047; //  将  low 赋值为 1047  结果为 0 为第一张图片位置
+    }
+  };
 
-    $("#li2").click(function () {
-        $("#childUl2").stop().slideToggle();
-    });
+  var timer = setInterval(function() { // 将计时器 赋值并声明  给 timer  为下面做准备
+    autoPlay() //  设置计时器   每 2000 毫秒  执行  autoPlay 方法
+  }, 2000);
 
+  $(".picList").mouseover(function() {
+    clearInterval(timer); // 鼠标进入  .picList 盒子时  执行  清除计时器  timer  停止动画
+  });
 
-    var picList = $("#picList >li");
-    var pointList = $("#pointList>li");
-    var low = 0; //  设置全局变量  为 轮播图 的  绝对定位 做准备
-
-    function autoPlay() { //   自动 轮播  方法/函数
-        low = low - 1047; // 将  low  的 值   每次  减少  1047  注：1047为一张图片的宽度
-        $(".picList").stop().animate({ //  拿到所有的 class 为  .picList 的  节点  先执行  停止动画  重置  动画队列  否则 动画会有冲突 按照队列执行 再执行动画 
-            "left": low + "px" //  将  low  变量的值  动态 赋值给  css left  注：.picList  为绝对定位 相对于 lunboComponent
-        });
-        if (low == -4188) { //  边界值 检测  当  Low 为 -4188 时  为 五张图片总长度  执行函数体语句
-            return low = 1047; //  将  low 赋值为 1047  结果为 0 为第一张图片位置 
-        }
-    };
-
-    var timer = setInterval(function () { // 将计时器 赋值并声明  给 timer  为下面做准备
-        autoPlay() //  设置计时器   每 2000 毫秒  执行  autoPlay 方法
+  $(".picList").mouseout(function() { //  鼠标离开  .picList 盒子时
+    clearInterval(timer); // 清除  计时器  timer  防止 计时器累加  导致 计时器 变化过快
+    timer = setInterval(function() { // 重新 执行  计时器  每 2000 毫秒  执行  autoPlay 方法
+      autoPlay()
     }, 2000);
-
-    $(".picList").mouseover(function () {
-        clearInterval(timer); // 鼠标进入  .picList 盒子时  执行  清除计时器  timer  停止动画
-    });
-
-    $(".picList").mouseout(function () { //  鼠标离开  .picList 盒子时
-        clearInterval(timer); // 清除  计时器  timer  防止 计时器累加  导致 计时器 变化过快
-        timer = setInterval(function () { // 重新 执行  计时器  每 2000 毫秒  执行  autoPlay 方法
-            autoPlay()
-        }, 2000);
-    });
+  });
 });
