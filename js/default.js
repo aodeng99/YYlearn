4ebc8a2f14f0319cb0f7da34b90bda9c284861a6
oDen7 <dengao99@126.com>
2019-03-03 13:38:22 +0800

    add  details.html
    fix default.css shop.css and default.js shop.js
    
    add details.css or details.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,23 @@
+$(document).ready(function () {
+    var lis = $("li");
+    var list = $("#sortList > li"); // 拿到  一级 li 
+    var childUl = $("#sortList > li >ul"); // 拿到  一级 li  下的 ul
+    var childList = $("#sortList > li >ul > li");
+
+    for (var i = 0; i < lis.length; i++) { // 循环遍历 
+        $(list[i]).attr("id", "li" + i); // 动态添加 属性  key：id  value： 名称  + 循环 i 变量
+        $(childUl[i]).attr("id", "childUl" + i); // 同上
+        $(childList[i]).attr("id", "childList" + i); // 同上
+    }
+});
+
+
+
+
+
+
+
+
 var string = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 /* 
@@ -7,6 +27,9 @@ var string = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "
 *    
 *   下列算法使用  二分排序 + 递归 ，大O 计法  log2（n）
 *
+* 使用方式
+*   var result = removeAdata(string, "a", 0, string.length);
+*   string.splice(result, 1);
 */
 function removeAdata(stringArr, queryString, low, high) {
     var mid = Math.floor((high - low) / 2 + low);
@@ -18,8 +41,3 @@ function removeAdata(stringArr, queryString, low, high) {
         return mid;
     }
 }
-
-var result = removeAdata(string, "a", 0, string.length);
-console.log(result);
-string.splice(result, 1);
-console.log(string);
--- js/shop.js
+++ js/shop.js
@@ -1,15 +1,4 @@
 $(document).ready(function () {
-    var lis = $("li");
-    var list = $("#sortList > li"); // 拿到  一级 li 
-    var childUl = $("#sortList > li >ul"); // 拿到  一级 li  下的 ul
-    var childList = $("#sortList > li >ul > li");
-
-    for (var i = 0; i < lis.length; i++) { // 循环遍历 
-        $(list[i]).attr("id", "li" + i); // 动态   添加 属性  key：id  value： 名称  + 循环 i 变量
-        $(childUl[i]).attr("id", "childUl" + i); // 同上
-        $(childList[i]).attr("id", "childList" + i); // 同上
-    }
-
     $("#li1").click(function () { // 根据 id 拿到 dom节点  做 事件（点击事件） 干什么事情  下面
         $("#childUl1").stop().slideToggle(); //  取反  显示时 点击之后  隐藏  反之  显示
     });
